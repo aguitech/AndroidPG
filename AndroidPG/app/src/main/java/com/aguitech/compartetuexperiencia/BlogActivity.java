8c514ce60a855465eea95ad081d1f464f1499be8
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-01-17 23:43:16 -0600

    Haciendo funcionar el menu

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/BlogActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/BlogActivity.java
@@ -1,10 +1,13 @@
 package com.aguitech.compartetuexperiencia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class BlogActivity extends AppCompatActivity {
@@ -24,6 +27,43 @@ public class BlogActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            i.setClass(BlogActivity.this, RegistroActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.action_eventos) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            i.setClass(BlogActivity.this, EventosActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/EventosActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/EventosActivity.java
@@ -1,11 +1,15 @@
 package com.aguitech.compartetuexperiencia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
 
 public class EventosActivity extends AppCompatActivity {
 
@@ -25,5 +29,44 @@ public class EventosActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            i.setClass(EventosActivity.this, RegistroActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.action_blog) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            i.setClass(EventosActivity.this, BlogActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
 }
