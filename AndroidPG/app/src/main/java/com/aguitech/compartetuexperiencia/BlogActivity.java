b8b5fbe87088751c9f2160112efe0c91806eba37
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-01-19 22:30:07 -0600

    Cambios en archivo

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/BlogActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/BlogActivity.java
@@ -1,6 +1,8 @@
 package com.aguitech.compartetuexperiencia;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +11,55 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class BlogActivity extends AppCompatActivity {
+
+
+    private TextView result;
+    private TextView blogTitulo1;
+    private TextView blogTitulo2;
+    private Button connect;
+    private EditText name;
+    private ProgressDialog pDialog;
+    private HashMap<String,String> data;
+    private String url = "http://emocionganar.com/admin/panel/webservice_blog_android.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+
+
+
+        result = (TextView) findViewById(R.id.result1);
+        blogTitulo1 = (TextView) findViewById(R.id.blogTitulo1);
+        blogTitulo2 = (TextView) findViewById(R.id.blogTitulo2);
+        result = (TextView) findViewById(R.id.result1);
+        connect = (Button) findViewById(R.id.connect1);
+        name = (EditText) findViewById(R.id.name1);
+
+
+        new connectPhp().execute();
+
+        /*
+        connect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                new connectPhp().execute();
+
+            }
+        });
+        */
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -86,6 +130,81 @@ public class BlogActivity extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class connectPhp extends AsyncTask<String, String, String> {
+
+        String getEdittextValue = name.getText().toString();
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new ProgressDialog(BlogActivity.this);
+            pDialog.setIndeterminate(false);
+            pDialog.setMessage("Connecting...");
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            //List <NameValuePair> args = new ArrayList<NameValuePair>();
+            //args.add(new BasicNameValuePair("name", getEdittextValue));//this is key and value to post data
+            data = new HashMap<String, String>();
+            //data.put("name", getEdittextValue);
+            data.put("name", getEdittextValue);
+            try{
+
+                //JSONObject json = jsonParser.makeHttpRequest(url, "POST", args);//to pass url, method, and args
+                //now connect using JSONParsr class
+                JSONObject json = HttpUrlConnectionParser.makehttpUrlConnection(url,data);
+                int succ = json.getInt("success");//get response from server
+                if(succ == 0){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else{
+                    JSONArray jsonArray = json.getJSONArray("blog");//get parent node
+
+                    JSONObject child = jsonArray.getJSONObject(0);//get first child value
+                    final String getValue = child.optString("titulo");
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //result.setText(getValue.toString());
+                            Toast.makeText(getApplicationContext(), "Funciona XD", Toast.LENGTH_SHORT).show();
+                            result.setText(getValue.toString());
+                            blogTitulo1.setText(getValue.toString());
+                            blogTitulo2.setText(getValue.toString());
+
+                            /*
+                            Intent i = new Intent();
+                            i.putExtra("Nombre", "Mi nombre es Hector");
+                            //i.setClass(MainActivity.this, PantallaActivity.class);
+                            //i.setClass(MainActivity.this, RegistroActivity.class);
+                            i.setClass(BlogActivity.this, RegistroActivity.class);
+                            startActivity(i);
+                            */
+                        }
+                    });
+                }
+            }catch(Exception e){
+
+            }
+
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String a){
+            super.onPostExecute(a);
+            pDialog.cancel();
+        }
     }
 
 }
