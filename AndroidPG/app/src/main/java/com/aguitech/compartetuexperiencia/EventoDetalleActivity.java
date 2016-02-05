9e6484a72853084ddc3fbd684105ca29055e5603
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-02-04 19:47:26 -0600

    Cambios

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/EventoDetalleActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/EventoDetalleActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -50,6 +52,73 @@ public class EventoDetalleActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_home) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", getNombreValue);
+            i.putExtra("ID", getIDValue);
+            i.putExtra("Dios", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            i.setClass(EventoDetalleActivity.this, MenuPrincipalActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.action_blog) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", getNombreValue);
+            i.putExtra("ID", getIDValue);
+            i.putExtra("Dios", "Mi nombre es Hector");
+            i.setClass(EventoDetalleActivity.this, BlogActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.action_trivias) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", getNombreValue);
+            i.putExtra("ID", getIDValue);
+            i.putExtra("Dios", "Mi nombre es Hector");
+            i.setClass(EventoDetalleActivity.this, TriviasActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.action_terminos_condiciones) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", getNombreValue);
+            i.putExtra("ID", getIDValue);
+            i.putExtra("Dios", "Mi nombre es Hector");
+            i.setClass(EventoDetalleActivity.this, TerminosCondicionesActivity.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.action_aviso_privacidad) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", getNombreValue);
+            i.putExtra("ID", getIDValue);
+            i.putExtra("Dios", "Mi nombre es Hector");
+            //i.setClass(EventosActivity.this, AvisoPrivacidadActivity.class);
+            i.setClass(EventoDetalleActivity.this, WebviewActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
