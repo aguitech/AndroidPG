33b90d1ed248039344069ade83775b5df07f4504
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-02-06 20:59:45 -0600

    Cambios

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/WebviewActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/WebviewActivity.java
@@ -3,19 +3,43 @@ package com.aguitech.compartetuexperiencia;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebviewActivity extends AppCompatActivity {
 
-    private WebView webView;
+    //private WebView webView;
+    private WebView mWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
 
-        webView = (WebView) findViewById(R.id.webview1);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://www.google.com");
+        mWebView = new WebView(this);
+        //mWebView.loadUrl("http://m.willowtreeapps.com/");
+        mWebView.loadUrl("http://google.com/");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                //return true;
+                return false;
+            }
+        });
+
+        this.setContentView(mWebView);
+
+        /*
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        */
 
 
     }
