b296ae92206ead832b5758fd51ff19c276cf06a4
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-02-19 23:30:15 -0600

    Cambios

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/GanadoresActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/GanadoresActivity.java
@@ -4,11 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class GanadoresActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class GanadoresActivity extends AppCompatActivity implements Download_data_ganador.download_complete {
+
+    public ListView list_ganadores;
+    public ArrayList<Countries> countries = new ArrayList<Countries>();
+    public ListAdapterGanador adapterGanador;
 
     public String getNombreValue = "";
     public String getIDValue = "";
@@ -31,6 +45,62 @@ public class GanadoresActivity extends AppCompatActivity {
 
             Toast.makeText(getApplicationContext(), getIDValue, Toast.LENGTH_SHORT).show();
         }
+
+        list_ganadores = (ListView) findViewById(R.id.list_ganadores);
+        adapterGanador = new ListAdapterGanador(this);
+        list_ganadores.setAdapter(adapterGanador);
+
+        list_ganadores.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            //public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //String position_val =  position.toString();
+                //Toast.makeText(getApplicationContext(), position_val, Toast.LENGTH_SHORT).show();
+                //int yourtag1 = Log.d("Yourtag", position);
+                String valor_posicion = String.valueOf(position);
+                /*
+                Toast.makeText(getApplicationContext(), "algun valor", Toast.LENGTH_SHORT).show();
+                HashMap<String, Object> obj_nuevo = (HashMap<String, Object>) adapter.getItem(position);
+                String name = (String) obj_nuevo.get("name");
+                Log.d("Yourtag", name);
+                Log.d("Position", adapter.getItem(position).toString());
+                */
+                //Toast.makeText(getApplicationContext(), , Toast.LENGTH_SHORT).show();
+                Log.d("ROWSELECT", "" + id);
+                Log.d("ROWSELECT2", "" + position);
+
+                Intent i = new Intent();
+                i.putExtra("Nombre", getNombreValue);
+                i.putExtra("ID", getIDValue);
+                i.putExtra("Dios", "Mi nombre es Hector");
+                i.putExtra("IDBlog", valor_posicion);
+                //i.setClass(GanadoresActivity.this, BlogDetalleActivity.class);
+                i.setClass(GanadoresActivity.this, BlogDetalleActivity.class);
+                startActivity(i);
+                /**
+                 HashMap<String, Object> obj = (HashMap<String, Object>) adapter.getItem(position);
+                 String name = (String) obj.get("name");
+                 Toast.makeText(getApplicationContext(), name, Toast.LENGTH_SHORT).show();
+                 *
+                 *
+                 Object obj = list.getAdapter().getItem(position);
+                 String valueID = obj.toString();
+
+                 Toast.makeText(getApplicationContext(), valueID, Toast.LENGTH_SHORT).show();
+                 */
+                //Log.d("MyLog", "Value is: "+value);
+                //String name = // how code to get name value.
+            }
+
+
+        });
+
+        Download_data_ganador download_data = new Download_data_ganador((Download_data_ganador.download_complete) this);
+        //download_data.download_data_from_link("http://www.kaleidosblog.com/tutorial/tutorial.json");
+        //download_data.download_data_from_link("https://emocionganar.com/admin/panel/webservice_evento_android.php");
+        //download_data.download_data_from_link("https://emocionganar.com/admin/panel/webservice_blog_android_nuevo.php");
+        download_data.download_data_from_link("https://emocionganar.com/admin/panel/webservice_ganadores_android.php");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -150,6 +220,32 @@ public class GanadoresActivity extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+    public void get_data(String data)
+    {
+
+        try {
+            JSONArray data_array=new JSONArray(data);
+
+            for (int i = 0 ; i < data_array.length() ; i++)
+            {
+                JSONObject obj=new JSONObject(data_array.get(i).toString());
+
+                Countries add=new Countries();
+                //add.name = obj.getString("titulo");
+                add.name = obj.getString("nombre");
+                add.code = obj.getString("puntos");
+
+                countries.add(add);
+
+            }
+
+            adapterGanador.notifyDataSetChanged();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
