58fcff8d8a200432153efa8f2b34bda22fb9d0f5
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-02-20 12:10:19 -0600

    Cambios

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/PreRegistroActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/PreRegistroActivity.java
@@ -1,11 +1,14 @@
 package com.aguitech.compartetuexperiencia;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class PreRegistroActivity extends AppCompatActivity {
 
@@ -30,9 +33,37 @@ public class PreRegistroActivity extends AppCompatActivity {
             }
         });
 
+        CargarPreferencias();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+    }
+
+    public void CargarPreferencias(){
+        //SharedPreferences mispreferencias = getSharedPreferences("PreferenciasUsuario", Context.NODE_PRIVATE);
+        SharedPreferences mispreferencias = getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+        //txtnombre.setText(mispreferencias.getString(“nombre”, “”));
+        Toast.makeText(getApplicationContext(), "Cargando Preferencias", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), mispreferencias.getString("Nombre", ""), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), mispreferencias.getString("ID", ""), Toast.LENGTH_SHORT).show();
+        String IDUsuario = mispreferencias.getString("ID", "");
+        Toast.makeText(getApplicationContext(), "Cargando Preferencias Iniciales", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), IDUsuario, Toast.LENGTH_SHORT).show();
+
+        if(IDUsuario != ""){
+            Toast.makeText(getApplicationContext(), "Tenemos sesion activa", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent();
+            i.putExtra("Nombre", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            //i.setClass(PreRegistroActivity.this, RegistroActivity.class);
+            i.setClass(PreRegistroActivity.this, MenuPrincipalActivity.class);
+            startActivity(i);
+
+        }
 
     }
 
