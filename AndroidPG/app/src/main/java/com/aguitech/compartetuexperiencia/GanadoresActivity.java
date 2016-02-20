a775a6846540fae5b6130b7e6ea360417560c56d
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-02-19 23:09:28 -0600

    Cambios

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/GanadoresActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/GanadoresActivity.java
@@ -2,13 +2,10 @@ package com.aguitech.compartetuexperiencia;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class GanadoresActivity extends AppCompatActivity {
@@ -38,6 +35,7 @@ public class GanadoresActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +44,7 @@ public class GanadoresActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
