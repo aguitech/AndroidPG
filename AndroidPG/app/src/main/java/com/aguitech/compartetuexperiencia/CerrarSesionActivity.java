4ec3a6563db1e24eb5ee73d30fb3f09121c50d47
Hector Aguilar <hectoraguilar.adesis@gmail.com>
2016-02-20 12:47:11 -0600

    Cambios

--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/CerrarSesionActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/CerrarSesionActivity.java
@@ -1,11 +1,15 @@
 package com.aguitech.compartetuexperiencia;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class CerrarSesionActivity extends AppCompatActivity {
 
@@ -13,6 +17,22 @@ public class CerrarSesionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cerrar_sesion);
+
+        SharedPreferences mispreferencias = getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mispreferencias.edit();
+        //String nombre = txtnombre.getText().toString();
+        //editor.putString(“nombre”, nombre);
+        editor.putString("ID", "");
+        editor.putString("Nombre", "");
+
+        editor.commit();
+
+
+        Toast.makeText(getApplicationContext(), "Cerrando Sesion", Toast.LENGTH_SHORT).show();
+
+        CargarPreferencias();
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +44,31 @@ public class CerrarSesionActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void CargarPreferencias(){
+        //SharedPreferences mispreferencias = getSharedPreferences("PreferenciasUsuario", Context.NODE_PRIVATE);
+        SharedPreferences mispreferencias = getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+        //txtnombre.setText(mispreferencias.getString(“nombre”, “”));
+        Toast.makeText(getApplicationContext(), "Cargando Preferencias", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), mispreferencias.getString("Nombre", ""), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), mispreferencias.getString("ID", ""), Toast.LENGTH_SHORT).show();
+        String IDUsuario = mispreferencias.getString("ID", "");
+        Toast.makeText(getApplicationContext(), "Cargando Preferencias Iniciales", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), IDUsuario, Toast.LENGTH_SHORT).show();
+
+        if(IDUsuario == ""){
+            Toast.makeText(getApplicationContext(), "Tenemos sesion activa", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent();
+            i.putExtra("Nombre", "Mi nombre es Hector");
+            //i.setClass(MainActivity.this, PantallaActivity.class);
+            //i.setClass(MainActivity.this, RegistroActivity.class);
+            //i.setClass(PreRegistroActivity.this, RegistroActivity.class);
+            i.setClass(CerrarSesionActivity.this, PreRegistroActivity.class);
+            startActivity(i);
+
+        }
+
     }
 
 }
--- AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/MenuPrincipalActivity.java
+++ AndroidPG/app/src/main/java/com/aguitech/compartetuexperiencia/MenuPrincipalActivity.java
@@ -244,6 +244,15 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             startActivity(i);
             return true;
         }
+        if (id == R.id.action_salir) {
+            Intent i = new Intent();
+            i.putExtra("Nombre", getNombreValue);
+            i.putExtra("ID", getIDValue);
+            i.putExtra("Dios", "Mi nombre es Hector");
+            i.setClass(MenuPrincipalActivity.this, CerrarSesionActivity.class);
+            startActivity(i);
+            return true;
+        }
 
 
         return super.onOptionsItemSelected(item);
